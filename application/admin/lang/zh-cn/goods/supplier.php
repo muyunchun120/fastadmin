2a4282e0b08389fff219465c205daf1f74c5c95a
muyunchun120 <wwwyalong@126.com>
2017-12-22 17:31:25 +0800

    添加供应商信息

---
+++ application/admin/lang/zh-cn/goods/supplier.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Id'  =>  'ID',
+    'Supplier_name'  =>  '供应商名称',
+    'Website'  =>  '网址',
+    'Remark'  =>  '备注',
+    'Createtime'  =>  '创建时间',
+    'Updatetime'  =>  '更新时间',
+    'Admin_id'  =>  '操作人'
+];
---
+++ public/assets/js/backend/goods/supplier.js
@@ -0,0 +1,53 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'goods/supplier/index',
+                    add_url: 'goods/supplier/add',
+                    edit_url: 'goods/supplier/edit',
+                    del_url: 'goods/supplier/del',
+                    multi_url: 'goods/supplier/multi',
+                    table: 'supplier',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'supplier_name', title: __('Supplier_name')},
+                        {field: 'website', title: __('Website')},
+                        {field: 'createtime', title: __('Createtime'), formatter: Table.api.formatter.datetime},
+                        {field: 'updatetime', title: __('Updatetime'), formatter: Table.api.formatter.datetime},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
